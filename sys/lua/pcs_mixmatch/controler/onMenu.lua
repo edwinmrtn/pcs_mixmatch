2ef474a4b4fa7460981e55a2cedab826ceafc814
edwinmrtn <martinedfr@yahoo.fr>
2015-09-20 20:50:12 +0200

    full working menu chooseteam protection

--- sys/lua/pcs_mixmatch/controler/onMenu.lua
+++ sys/lua/pcs_mixmatch/controler/onMenu.lua
@@ -9,8 +9,7 @@ function onMenu(id,title,button)
 			 if(title == theMenuList:getMenuList():Get(i):getTitre())then 
 			 	for j=1,theMenuList:getMenuList():Get(i):NumbersBoutons() do
 			 		if(button == 0 and button == theMenuList:getMenuList():Get(i):getBoutons():Get(j):getId()) then
-			 			menu(id,theMenuList:getMenuList():Get(i):Show())
-			 			parse("msg okok"..id.." "..theMenuList:getMenuList():Get(i):Show())
+			 			timer(0,"atimer",id)
 			 		elseif(button == theMenuList:getMenuList():Get(i):getBoutons():Get(j):getId()) then
 			 			local fonctionName = theMenuList:getMenuList():Get(i):getBoutons():Get(j):getFonction()
 			 			_G[fonctionName](id) --thanks http://stackoverflow.com/questions/1791234/lua-call-function-from-a-string-with-function-name 
@@ -19,4 +18,14 @@ function onMenu(id,title,button)
 			 	end
 			 end
 	end	
+end
+
+function atimer(id)
+	local theMenuList = MenuList(0)
+	for i=1,theMenuList:NumbersMenus() do
+		 if("Choose your team" == theMenuList:getMenuList():Get(i):getTitre())then 
+			menu(tonumber(id),theMenuList:getMenuList():Get(i):Show())
+		 end
+	end
+	
 end
