8f35a3fd53e86ff54a3ebb60d24e7a55275e485f
edwinmrtn <martinedfr@yahoo.fr>
2015-11-12 19:11:04 +0100

    working basic sub yay

--- controler/onsubstitute.lua
+++ controler/onsubstitute.lua
@@ -15,24 +15,33 @@ function onSubstitute(id)
     elseif(team:getName() == "ct")then
         parse("makect "..player:getId())
     end
+                 
+    for j=1,aMixList:NumbersMixs() do
+            for k=1, aMixList:getMixList():Get(j):getNumberRegist() do
+                    if (id == aMixList:getMixList():Get(j):getRegistPlayers():Get(k):getId()) then
+                        aMixList:getMixList():Get(j):removeRegistPlayer(aMixList:getMixList():Get(j):getRegistPlayers():Get(k))
+                    end
+            end
+    end 
 
 end
 
 function setspec(id)
     for k=1, aMixList:NumbersMixs() do
         if(aMixList:getMixList():Get(k):getState() ~= "preparation") then
-    		for i=1, aMixList:getMixList():Get(k):getTeams():Get(1):NumbersPlayers() do
+           
+            for i=1, aMixList:getMixList():Get(k):getTeams():Get(1):NumbersPlayers() do
        			if id == aMixList:getMixList():Get(k):getTeams():Get(1):getPlayers():Get(i):getId() then
                     parse("makespec "..id)
        				aMixList:getMixList():Get(k):getTeams():Get(1):removePlayer(aMixList:getMixList():Get(k):getTeams():Get(1):getPlayers():Get(i))
-       			    return aMixList:getMixList():Get(k):getTeams():Get(1)
+                    return aMixList:getMixList():Get(k):getTeams():Get(1)
                 end 
             end
     		for i=1, aMixList:getMixList():Get(k):getTeams():Get(2):NumbersPlayers() do
        			if id == aMixList:getMixList():Get(k):getTeams():Get(2):getPlayers():Get(i):getId() then
                     parse("makespec "..id)
        				aMixList:getMixList():Get(k):getTeams():Get(2):removePlayer(aMixList:getMixList():Get(k):getTeams():Get(2):getPlayers():Get(i))
-       				return aMixList:getMixList():Get(k):getTeams():Get(2)
+                    return aMixList:getMixList():Get(k):getTeams():Get(2)
        			end
             end
         end
@@ -43,12 +52,13 @@ function compareList(liste1, liste2)
   for i = 1, liste1:Size() do
     local found = false
     for j = 1, liste2:Size() do
-      if(liste1:Get(i):getId() == liste2:Get(j):getId()) then
-        found = true
-        break
-      end
+        if(liste1:Get(i):getId() == liste2:Get(j):getId()) then
+            found = true
+        end
     end
-    return liste1:Get(i)
+    if found == false then
+        return liste1:Get(i)
+    end
   end
 end
 
